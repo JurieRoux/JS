706c6ac768e8f20ce192675ed8e9bedb47592e17
JurieRoux <157107427+JurieRoux@users.noreply.github.com>
2024-03-11 18:40:03 +0200

    11 MArch

---
+++ Chapter12/class.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// let text = "I love NodeJS, React or JavaScript!";
+
+// //// input the i to make it not be case sensitive
+// // console.log(text.match(/javascript/));
+// /// example of i below
+// // console.log(text.match(/javascript/i));
+// console.log(text.match(/javascript|nodejs|react/i));
+
+// let text = "e";
+// console.log(text.match(/[abcd]/));
+
+// let text = "4";
+// // console.log(text.match(/[A-Z]/i));
+// console.log(text.match(/[a-zA-Z0-9]/));
+
+// let text = "Just do it";
+// console.log(text.match(/./g));
+
+// let text = "Just do it.";
+// console.log(text.match(/\./));
+// console.log(text.match(/\./g));
+
+// let text = "I'm 29 years old.";
+// console.log(text.match(/\d/g));
+
+// let text = "Coding is a lot of fun!";
+// console.log(text.match(/\s/g));
+
+// let text = "In the end or at the beginning?";
+// console.log(text.match(/\bin/gi));
+
+// let text = "I love JavaScript!";
+// let text2 = "I very much Dislike spiders"
+// console.log(text.match(/(love|dislike)\s(javascript|spiders)/gi));
+// console.log(text2.match(/(love|dislike)\s(javascript|spiders)/gi));
+
+// // /// Aplha numeric
+// let text = "I love JavaScript!";
+// console.log(text.match(/[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]/g));
+
+// let text = "You are doing great!";
+// console.log(text.match(/n?g/gi));
+
+// let text = "123123123";
+// console.log(text.match(/(123)+/));
+
+/////// email input form
+// let emailPattern = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/g;
+// let validEmail = "jurie_roux@email.com";
+// let validEmail2 = "jurieroux@email.com";
+// let validEmailTest = "jr@email.co.za";
+// let invalidEmail = "maaikemail@.com";
+// console.log(validEmail.match(emailPattern));
+// console.log(validEmail2.match(emailPattern));
+// console.log(validEmailTest.match(emailPattern));
+// console.log(invalidEmail.match(emailPattern));
+
+// function test(a, b, c) {
+//     console.log("first:", arguments[0]);
+//     console.log("second:", arguments[1]);
+//     console.log("third:", arguments[2]);
+// }
+// test("fun", "js", "secrets");
+
+// let x;
+// x = 5;
+// console.log(x);
+
+// y = 5;
+// console.log(y);
+// var y;
+
+
+
+
+
+
